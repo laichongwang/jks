7525a933c8a3ea3f0cfe011a4f4dd48562d89e22
86138 <644654471@qq.com>
2021-10-10 21:16:00 +0800

    jenkins测试

---
+++ src/main/java/com/example/jkst/JkstApplication.java
@@ -0,0 +1,13 @@
+package com.example.jkst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JkstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jkst/JkstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jkst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
